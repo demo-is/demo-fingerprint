5f066f81f5afe23f4286f2c1f913b6f7a63b39ef
Maksim Kirilko <maksim.kirilko@intellectsoft.net>
2018-02-26 10:02:30 +0300

    Init commit

---
+++ main.js
@@ -0,0 +1,27 @@
+(function () {
+    new Fingerprint2().get(function(result, components) {
+        checkFingerprint(result);
+        setHashValueUI(result);
+        setHashValueLS(result);
+        console.log(result, components);
+    });
+
+    function setHashValueUI(value) {
+        document.getElementById('fingerprint').innerText = value;
+    }
+
+    function setHashValueLS(value) {
+        window.localStorage.setItem('_fingerprint', value)
+    }
+
+    function checkFingerprint(value) {
+        const fpLS = window.localStorage.getItem('_fingerprint');
+        if (!fpLS) {
+            return;
+        }
+        const message = fpLS === value ?
+            '<span class="success">ID matches!</span>' :
+            '<span class="failed">ID does not match.</span><p>New: ' + value + '.</p><p>Old: ' + fpLS + '</p>';
+        document.getElementById('result').innerHTML = message;
+    }
+})();
